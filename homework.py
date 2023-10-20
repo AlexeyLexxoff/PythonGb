f64275fce90b29bdba86cda2332c79d69ad16c87
AlexeyLexxoff <djlexxoff@gmail.com>
2023-10-20 07:02:55 +0300

    new lessons

---
+++ homework.py
@@ -0,0 +1,59 @@
+# # n = 123
+
+# # n1 = n // 100
+# # n2 = (n % 100) // 10
+# # n3 = n % 10
+# # res =  n1 + n2 + n3
+# # print (res)
+
+# # n = 6 -> 1 4 1  
+# # n = 24 -> 4 16 4    
+# # n = 60 -> 10 40 10 
+# # n = 24 
+
+# # n = int(n) // 3
+# # print(n//2, n *2, n //2)
+
+# # n = 123456
+# # n = str(n)
+
+# # s1 = sum(int(digit) for digit in n[:3])
+# # s2 = sum(int(digit) for digit in n[-3:])
+# # if s1 == s2:
+# #     print('yes')
+# # else:
+# #     print('no')
+
+
+# # a = int(input("введите a "))
+# # b = int(input("введите b "))
+# # c = int(input("введите c "))
+# # # print(round(a / 2) + round(b / 2) + round(c / 2))
+
+# # print((a % 2 + a//2) + (b % 2 + b//2) + (c % 2 + c//2))
+
+# # a = 3
+# # b = 5
+# # c = 10
+# # if c < a * b and ((c % b == 0)) or (c % b == 0):
+# #     print("yes")
+# # else:
+# #     print ("no")
+
+
+# a = 2
+# b = 6
+# c = 10
+
+# if a * b == c:
+#     print("yes")
+# else:
+#     print("no")
+
+# r = range(0, 1000, 22,)
+# for i in r:
+#     print(i)
+
+# a = 'qwerty'
+# for i in a:    
+#     print(i)
